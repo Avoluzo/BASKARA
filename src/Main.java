ded468a051c07dfb8984bd650aedec82d8f4b913
Avoluzo <guguavoluzo@gmail.com>
2023-01-29 21:19:02 -0300

    teste inicial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        import Locale;
+        import Scanner;
+
+        public class Main {
+
+            public static void main(String[] args) {
+
+                Locale.setDefault(Locale.US);
+                Scanner sc = new Scanner(System.in);
+
+
+                double b;
+                double c;
+
+                double a = sc.nextDouble();
+                b = sc.nextDouble();
+                c = sc.nextDouble();
+
+                double delta = b*b - 4.0 *a * c;
+
+
+                if (a == 0 || delta < 0.0  ) {
+                    System.out.println("IMPOSSIVEL CALCULAR");
+                }
+                else {
+                    double r1 = (-b + Math.sqrt(delta)) / (2.0 * a);
+                    double r2 = (-b - Math.sqrt(delta)) / (2.0 * a);
+                    System.out.printf("R1 = %.5f%n" , r1);
+                    System.out.printf("R2 = %.5f%n" , r2);
+                }
+
+
+                sc.close();
+
+
+            }
+
+        }
+
     }
 }
